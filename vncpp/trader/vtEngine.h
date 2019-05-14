23a49c9b40c6aa136dd393a1f27ca4edb9b1d74c
qq184473091 <184473091@qq.com>
2019-05-14 23:15:38 +0800

    mainEngine

--- vncpp/trader/vtEngine.h
+++ vncpp/trader/vtEngine.h
@@ -4,6 +4,9 @@
 #include <memory>
 #include <map>
 #include "eventEngine.h"
+#include "vtObject.h"
+#include "vtGateway.h"
+
 
 class MainEngine
 {
@@ -21,6 +24,7 @@ protected:
 public:
 	MainEngine(EventEnginePtr ee)
 		: eventEngine(ee)
+		, dataEngine(std::make_shared<DataEngine>(ee))
 	{
 	}
 
@@ -34,6 +38,94 @@ public:
 		appDict[app->getName()] = app;
 	}
 
+	GatewayPtr getGateway(const std::string& gwName)
+	{
+		auto i = gatewayDict.find(gwName);
+		if(i != gatewayDict.end())
+			return i->second;
+		return GatewayPtr();
+	}
+
+	void connect(const std::string& gwName)
+	{
+		auto gw = getGateway(gwName);
+		if(gw)
+			gw->connect();
+	}
+
+	void subscribe(SubscribeRequestPtr req, const std::string& gwName)
+	{
+		auto gw = getGateway(gwName);
+		if(gw)
+			gw->subscribe(req);
+	}
+
+	OrderID sendOrder(OrderRequestPtr req, const std::string& gwName)
+	{
+		if(rmEngine && !rmEngine->checkRisk(req, gwName))
+		{
+			return OrderID();
+		}
+
+		auto gw = getGateway(gwName);
+		if(gw)
+		{
+			auto orderID = gw->sendOrder(req);
+			dataEngine->updateOrderReq(req, orderID);
+			return orderID;
+		}
+		else
+			return OrderID();
+	}
+
+	void cancelOrder(CancelOrderRequestPtr req, const std::string& gwName)
+	{
+		auto gw = getGateway(gwName);
+		if(gw)
+		{
+			gw->cancelOrder(req);
+		}
+	}
+
+	AccountPtr qryAccount(const std::string& gwName)
+	{
+		auto gw = getGateway(gwName);
+		if(gw)
+		{
+			return gw->qryAccount();
+		}
+		else
+			return AccountPtr();
+	}
+
+	std::vector<PositionPtr> qryPosition(const std::string& gwName)
+	{
+		auto gw = getGateway(gwName);
+		if(gw)
+		{
+			return gw->qryPosition();
+		}
+		else
+			return std::vector<PositionPtr>();
+	}
+
+
+	void exit()
+	{
+		for(auto i : gatewayDict)
+		{
+			i.second->close();
+		}
+
+		for(auto j : appDict)
+		{
+			i.second->stop();
+		}
+
+		dataEngine->saveContracts();
+
+		eventEngine->stop();
+	}
 };
 
 typedef std::shared_ptr<MainEngine> MainEnginePtr;
